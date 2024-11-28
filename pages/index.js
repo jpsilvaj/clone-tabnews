84252a46d279e63c7d6a7567f815a80925b4a285
João Paulo da Silva Júnior <jpsilvaj@gmail.com>
2024-11-28 01:57:50 +0000

    Add lint in project

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home(){
-    return <h1>Você ganhou uma noite romântica comigo! Dê um sorrisinho se aceita!</h1>
+function Home() {
+  return (
+    <h1>Você ganhou uma noite romântica comigo! Dê um sorrisinho se aceita!</h1>
+  );
 }
 
 export default Home;
